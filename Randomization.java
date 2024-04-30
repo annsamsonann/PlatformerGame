5b788a0475e345a6e3dd730fb086175eb510380a
annsamsonann <165697505+annsamsonann@users.noreply.github.com>
2024-04-30 13:23:06 -0400

    Update Randomization.java

--- Randomization.java
+++ Randomization.java
@@ -8,23 +8,27 @@ public class Randomization {
     //Upper and lower range for the width of platforms
     final static double MaxWidth = 300;
     final static double MinWidth = 100;
-    
+
+    //creates a random interval within a range
     public static double randomInteval(){
         Random random = new Random();
         double interval = random.nextDouble(MaxInterval)+MinInterval;
         return interval;
     }
+
+    //creates an xCoordinate based on the random interval
     public static double xCoordNext(double randomInteval, double width){
         double xCoord = movedCoord+randomInteval;
         movedCoord += (randomInteval+width);
         return xCoord;
     }
-    
+    //x corrdinate for the first platfom on the level
     public static double xCoordFirst(double randomInteval, double width){
         double xCoord = 0+ randomInteval;
         movedCoord = movedCoord + (xCoord+width);
         return xCoord;
     }
+    //created random width
     public static double width(){
         Random random = new Random();
         double width = random.nextDouble(MaxWidth)+MinWidth;
